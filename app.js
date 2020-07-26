c1500478103dfbb23222726f7a75176f275e5a31
Eihab <mastereihab@gmail.com>
2020-07-26 17:09:00 +0100

    added basic playback functionality
    added play, pause, and stop functionaility to the video controls

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("APP IS LIVE!")
+const video = document.getElementById("video");
+const playBtn = document.getElementById("play");
+const stopBtn = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play & Pause
+function toggleVideoStatus() {
+    video.paused ? video.play() : video.pause();
+}
+
+// Update play and pause button
+function updatePlayButton() {
+    video.paused ? playBtn.innerHTML = '<i class="fa fa-play fa-2x" aria-hidden="true"></i>' : playBtn.innerHTML = '<i class="fa fa-pause fa-2x" aria-hidden="true"></i>'
+}
+
+// Update progress 
+function updateProgress() {
+    return true;
+}
+
+// Set Video Time
+function setVideoProgess(){
+    return true;
+}
+
+// Stop Video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+// Event Listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayButton);
+video.addEventListener("play", updatePlayButton);
+video.addEventListener("timeupdate", updateProgress);
+
+playBtn.addEventListener("click",toggleVideoStatus);
+stopBtn.addEventListener("click",stopVideo);
+
+progress.addEventListener("change", setVideoProgess);
